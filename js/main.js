5479993a8334adc426ddd74aa05dab8f0fde3a64
Metehan Saral <metehansaral@gmail.com>
2020-11-23 14:26:38 +0300

    location data has been added and minor improvements have been made

--- js/main.js
+++ js/main.js
@@ -28,11 +28,15 @@ function fakeData() {
         temp3: Math.floor(Math.random() * 101),
         speed: Math.floor(Math.random() * 181),
         battery: Math.floor(Math.random() * 101),
+
+        geo1: '4' + Math.floor(Math.random() * 5),
+        geo2: '2' + Math.floor(Math.random() * 10),
         
         speed_lu: time,
         battery_lu: time,
         voltage_lu: time,
         temp_lu: time,
+        geo_lu: time
     });
 }
 
@@ -57,6 +61,49 @@ function parseScreen(data) {
     $('#battery-lu').html(data.battery_lu);
     $('#voltage-lu').html(data.voltage_lu);
     $('#temp-lu').html(data.temp_lu);
+
+    setLocation(data.geo1, data.geo2, data.speed, data.voltage, data.geo_lu);
+}
+
+/**
+ * Aşağıda (ready kısmında) belirttiğim üzere sonradan güncelleyebilmek adına haritayı 'evaCarLocation' olarak ayarladım
+ * Şimdi veritabanından gelen veriye göre mapi düzenleme kısmını yapıyoruz
+ */
+function setLocation(lon, lat, speed, voltage, last_update) {
+    /**
+     * Haritadaki tüm objeleri (işaretçileri falan) kaldırıyor
+     * Nedeni ise yeni bir işaretçi oluşturacağımız için eskisi ile çakışmamasını sağlamak
+     */
+    evaCarLocation.geoObjects.removeAll()
+
+    /**
+     * Yeni bir placemark (işaretçi) oluşturuyoruz fonksiyonun istediği lon ve lat bilgisine yerleştirilecek bu işaretçi
+     * (ymaps.Placemark( ... ) burada belirtiyoruz long ve lat bilgisini)
+     * 
+     * Ve sonra kolay erişilebilirlik açısından oluşturduğumuz placemarkın yanına hız ve voltaj bilgisini yazdırıyoruz
+     * Böylece ekranı sürekli aşağıya veya yukarıya kaydırma gereksimi kalkıcak
+     * 
+     * preset ise işaretçinin simgesi bunu Yandex'in kendi dökümantasyonunda belirttiği şeyler üzerinden seçtim
+     * Başka işaretçilerde mevcut
+     */
+	var myGeoObject = new ymaps.Placemark([lon, lat], {
+		iconCaption: 'Speed: ' + speed + ', Voltage: ' + voltage
+	}, {
+		preset: 'islands#blueGlyphCircleIcon',
+		iconGlyph: 'car'
+	});
+    
+    /**
+     * Oluşturduğumuz işaretçiyi haritaya ekliyoruz
+     */
+    evaCarLocation.geoObjects.add(myGeoObject);
+
+    /**
+     * İşaretçiyi ekledikten sonra kullanıcının ekranının tam merkezine gelicek bir şekilde işaretçiyi konumlatıyoruz
+     */
+    evaCarLocation.setCenter([lon, lat]);
+    
+    $('#geo-lu').html(last_update)
 }
 
 /**
@@ -87,4 +134,35 @@ $(document).ready(function() {
         fakeData()
     }, 1000);
 
+    /**
+     * Yandex'in kendi açıkladığı talimatlara göre yapılmış harita çağırma kodu
+     * ymaps hazır olduğunda Körfez Pistinde başlayan harita oluşturuyor
+     * Oluşturduğu haritayı ise 'evaCarLocation' olarak ayarladım böylece daha sonra konum değiştiğinde dinamik olarak 
+     * İşaretçiyi değiştirebileceğiz
+     */
+    ymaps.ready(function () {
+        window.evaCarLocation = new ymaps.Map("map", {
+            center: [40.743865, 29.782439],
+            zoom: 16
+        }, {
+            searchControlProvider: 'yandex#search'
+        }),
+
+        window.myGeoObject = new ymaps.GeoObject({
+            geometry: {
+                type: "Point",
+                coordinates: [40.743865, 29.782439]
+            }
+        }, {
+            preset: 'islands#circleIcon',    
+            iconCaption: 'Hız: ',
+            draggable: true
+        }),
+
+        myPieChart = new ymaps.Placemark([
+            55.847, 
+        ]);
+
+        evaCarLocation.geoObjects.add(myGeoObject)
+    });
 });
